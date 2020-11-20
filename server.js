50984b80783f3d756edb1297bcbb08a432cc1f11
Farhana Yasmin <farhana@zool.in>
2020-11-20 20:54:32 +0530

    added status code

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let app = express()
 app.get('/', (req,res) =>{
-    res.send("Welcome to the app")
+    res.status(200).send("Welcome ")
 })
 app.listen(3000, (err) => {
     if (err)
